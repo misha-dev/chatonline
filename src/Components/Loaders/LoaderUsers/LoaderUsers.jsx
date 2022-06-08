7aed35daa8bb880a3cb4de8515a89005696e96c5
Misha-dev <shabatinmichael@mail.ru>
2022-06-08 19:24:59 +0500

    replaced loader for users for responsiveness

--- src/Components/Loaders/LoaderUsers/LoaderUsers.jsx
+++ src/Components/Loaders/LoaderUsers/LoaderUsers.jsx
@@ -3,18 +3,7 @@ import cl from "./LoaderUsers.module.css";
 export const LoaderUsers = () => {
   return (
     <div className={cl.loaderWrapper}>
-      <div className={cl.ldsDefault}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className={cl.ldsCircle}>
         <div></div>
       </div>
     </div>
